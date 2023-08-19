f8d18263b241d86224193b0acb8a0e8a6894e0b2
yuri <74125993+jeongYuri@users.noreply.github.com>
2023-08-19 13:01:09 +0900

    [unrated] Title: 수 조작하기 2, Time: 12.99 ms, Memory: 12.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,14 @@
+def solution(numLog):
+    operations = ["w", "s", "d", "a"] 
+    result = "" 
+    for i in range(1,len(numLog)):
+        diff = numLog[i]-numLog[i-1]
+        if diff==1:
+            result+='w'
+        elif diff==-1:
+            result +='s'
+        elif diff==10:
+            result +='d'
+        elif diff==-10:
+            result +='a'
+    return result
