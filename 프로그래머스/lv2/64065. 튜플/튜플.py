4696a2f432f61e6ab078e353b6a00772c6a66217
yuri <74125993+jeongYuri@users.noreply.github.com>
2023-09-21 15:21:14 +0900

    [level 2] Title: 튜플, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,14 +1,11 @@
 def solution(s):
     answer = []
-    s = s[2:-2].replace("{","").replace(","," ").split("}")
+    s = s[2:-2].replace("}","").replace(","," ").split("{")
     for i, v in enumerate(s):
         s[i] = v.split()
     s.sort(key = lambda x:len(x))
     for i in s:
         for j in range(len(i)):
             if int(i[j]) not in answer:
-                answer.append(int(i[j]))
+                answer.append(int(i[j]) )
     return answer
-       
-
-           
