164fda6ae8105d9b3532944e7f6e5bb1b071c304
yuri <74125993+jeongYuri@users.noreply.github.com>
2023-10-26 15:11:23 +0900

    [level 2] Title: 전화번호 목록, Time: 84.28 ms, Memory: 28 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,8 @@
+def solution(phone_book):
+    answer = True
+    pb = sorted(phone_book)
+    
+    for p1, p2 in zip(pb, pb[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
