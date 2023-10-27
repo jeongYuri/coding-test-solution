059cb78a8fd270f3201fce3fa2c6829a9109741a
yuri <74125993+jeongYuri@users.noreply.github.com>
2023-10-27 16:19:10 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,26 @@
+from collections import deque
+def solution(begin, target, words):
+    answer = 0
+    if target not in words:
+        return 0
+    return bfs(begin, target, words)
+
+def bfs(begin,target,words):
+    queue = deque()
+    queue.append([begin,0]) # 0단계 초기화
+    
+    while queue :
+        now, step = queue.popleft()
+        if now == target :
+            return step
+        
+        for word in words:
+            count = 0
+            for i in range(len(now)):
+                if now[i]!= word[i]:
+                    count +=1
+            if count ==1:
+                queue.append([word,step+1])    
+                
+            
+   
