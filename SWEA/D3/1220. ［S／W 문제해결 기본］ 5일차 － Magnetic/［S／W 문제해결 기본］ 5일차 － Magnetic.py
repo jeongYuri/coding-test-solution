9a6141b5b7b0328a187cfbfcc2e3c5301910edb8
yuri <74125993+jeongYuri@users.noreply.github.com>
2023-11-17 22:40:27 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 234 ms, Memory: 62,780 KB -BaekjoonHub

---
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -0,0 +1,14 @@
+for tc in range(1,11):
+    n = int(input())
+    arr = [list(map(int,input().split()))for _ in range(n)]
+    count = 0
+    for i in range(n):
+        flag = 0
+        for j in range(n):
+            if arr[j][i]==1:
+                flag = 1
+            elif arr[j][i]==2:
+                if flag:
+                    count +=1
+                    flag = 0
+    print(f'#{tc} { count}')
