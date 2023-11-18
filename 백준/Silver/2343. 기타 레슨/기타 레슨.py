5ef0300020c37ae0225505bacbcee312bf58fce2
yuri <74125993+jeongYuri@users.noreply.github.com>
2023-11-18 20:05:44 +0900

    [Silver I] Title: 기타 레슨, Time: 656 ms, Memory: 42204 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -0,0 +1,26 @@
+n,m = map(int,input().split())
+a = list(map(int,input().split()))
+start = 0
+end = 0
+
+for i in a:
+    if start<i:
+        start = i
+    end +=i
+
+while start<=end:
+    middle = int((start+end)/2)
+    sum = 0
+    count = 0
+    for i in range(n):
+        if sum+a[i]>middle:
+            count +=1
+            sum = 0
+        sum+=a[i]
+    if sum!=0:
+        count+=1
+    if count>m:
+        start = middle+1
+    else:
+        end = middle -1
+print(start)
