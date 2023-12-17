aca1235dde1049f99cefa7c253aa50f1905390ca
yuri <74125993+jeongYuri@users.noreply.github.com>
2023-12-17 17:34:16 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,25 @@
+n = int(input())
+for i in range(n):
+    text = input()
+    stack = []
+    if text == ".":
+        break
+    for i in text:
+        if i == '[' or i == '(':
+            stack.append(i)
+        elif i == ']':
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(']')
+                break
+        elif i == ')':
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(')')
+                break
+    if len(stack) == 0:
+        print('YES')
+    else:
+        print('NO')
