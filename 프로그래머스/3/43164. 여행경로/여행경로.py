82e6d1c00d5c1c4229127fc99a74aee5c48d7e7a
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-01-08 20:37:41 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,19 @@
+def solution(tickets):
+    answer = []
+    arr = dict()
+    for start,end in tickets:
+        if start in arr:
+            arr[start].append(end)
+        else:
+            arr[start] = [end]
+    for a in arr:
+        arr[a].sort(reverse = True)
+    st = ["ICN"]
+    while st:
+        top = st[-1]
+        if (top not in arr) or (not arr[top]):
+            answer.append(st.pop())
+        else:
+            st.append(arr[top].pop())
+    
+    return answer[::-1]
