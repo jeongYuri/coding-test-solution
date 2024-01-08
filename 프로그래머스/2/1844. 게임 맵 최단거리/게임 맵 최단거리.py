58bd1dd45faed5a725884764ac815f0b71557e54
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-01-08 20:01:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.51 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,22 @@
+from collections import deque
+def solution(maps):
+    answer = 0
+    q=deque()
+    q.append((0,0))
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            n = len(maps)
+            m = len(maps[0])
+            if 0<=nx<n and 0<=ny<m and maps[nx][ny]==1:
+                maps[nx][ny] = maps[x][y]+1
+                q.append((nx,ny))
+                
+    answer = maps[n-1][m-1]
+    if answer==1:
+        return -1
+    return answer
