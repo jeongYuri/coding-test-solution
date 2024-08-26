a52b78aecce6d24e435dfacbdf6c7521a6213cde
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-08-26 22:01:43 +0900

    [Bronze III] Title: 알람 시계, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,11 +1,1 @@
-import sys
-input = sys.stdin.readline
-
-h, m = map(int, input().split())
-if m < 45:
-    if h == 0:
-        print(23, m + 15)
-    else:
-        print(h - 1, m + 15)
-else:
-    print(h, m - 45)
+import sys; h, m = map(int, sys.stdin.readline().split()); print((h-1 if m<45 else h) if h != 0 or m >= 45 else 23, m+15 if m<45 else m-45)
