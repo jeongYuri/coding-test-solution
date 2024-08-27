87165646ebd26e0b1149cc0003f6d4fc57dc559e
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-08-27 14:46:59 +0900

    [Silver II] Title: 랜선 자르기, Time: 56 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,13 +1,11 @@
 import sys
 input = sys.stdin.readline
 k,n = map(int,input().split())
-lan = [int(input()) for _ in range(k)]
-start, end = 1, max(lan)
-while start<=end:
+lensun = [int(input()) for _ in range(k)]
+start, end = 1, max(lensun)
+while start<= end:
     mid = (start+end)//2
-    line = 0
-    for i in lan:
-        line += i//mid
+    line = sum(i//mid for i in lensun)
     if line>=n:
         start = mid+1
     else:
