ac20d41b11586f8f72d15b867c1758e2c2064ef3
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-08-31 21:58:38 +0900

    [Silver I] Title: IOIOI, Time: 364 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,20 +1,23 @@
 import sys
 input = sys.stdin.readline
+N = int(input().strip())  
+M = int(input().strip())  
+S = input().strip()      
 
-def find(s,text):
-    count,start = 0,0
+P = 'IOI'  
+cnt = 0    
+answer = 0
+i = 0      
 
-    while True:
-        start = s.find(text,start)
-        if start == -1:
-            break
-        count +=1
-        start +=1
-    return count
+while i < (M - 1):
+    if S[i:i+3] == P:
+        i += 2
+        cnt += 1
+        if cnt == N:
+            answer += 1
+            cnt -= 1
+    else:
+        i += 1
+        cnt = 0
 
-n = int(input())
-m = int(input())
-s = input()
-text = 'I'+'OI'*(n-1)+'OI'
-ans = find(s,text)
-print(ans)
+print(answer)
