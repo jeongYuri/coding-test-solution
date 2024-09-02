4fe392cca36c74bb21311b88e0d24c5263dc93e1
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-09-02 16:57:39 +0900

    [Silver I] Title: 정수 삼각형, Time: 88 ms, Memory: 40628 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -9,10 +9,7 @@ def path_sum(triangle):
             dp[i][j] += max(dp[i + 1][j], dp[i + 1][j + 1])
     return dp[0][0]
 
-def main():
-    n = int(input().strip())  # 첫 번째 줄에서 n을 읽어옵니다.
-    triangle = [list(map(int, input().split())) for _ in range(n)]
-    print(path_sum(triangle))
+n = int(input().strip())  
+triangle = [list(map(int, input().split())) for _ in range(n)]
+print(path_sum(triangle))
 
-if __name__ == "__main__":
-    main()
