5a14b217b14cf1746d2209d9803b3e12f1113cd3
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-09-22 00:24:31 +0900

    [Silver V] Title: 셀프 넘버, Time: 100 ms, Memory: 13972 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean[] arr = new boolean[10001];
+
+        for (int i = 1; i <= 10000; i++) {
+            int generatedNum = generateSelfNumber(i);
+            if (generatedNum <= 10000) {
+                arr[generatedNum] = true;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < 10000; i++) {
+            if (!arr[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+
+    static int generateSelfNumber(int n) {
+        int sum = n;
+        while (n != 0) {
+            sum += n % 10; 
+            n /= 10;
+        }
+        return sum;
+    }
+}
