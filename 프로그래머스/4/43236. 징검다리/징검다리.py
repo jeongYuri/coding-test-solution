30dc77886fcd03ea647284f31921b4893ce6dc62
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-10-16 22:33:07 +0900

    [level 4] Title: 징검다리, Time: 177.60 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -2,20 +2,20 @@ def solution(distance, rocks, n):
     answer = 0
     rocks.sort()
     rocks.append(distance)
-    start,end = 0,distance
+    start , end = 0,distance
     while start<=end:
         mid = (start+end)//2
         cnt = 0
         current = 0
         for rock in rocks:
-            if rock - current<mid:
-                cnt +=1
+            if rock - current < mid:
+                cnt +=1 #삭제해야하는 바위수
             else:
                 current = rock
         if cnt>n:
             end = mid-1
         else:
-            answer= mid
+            answer = mid
             start = mid+1
                 
     return answer
