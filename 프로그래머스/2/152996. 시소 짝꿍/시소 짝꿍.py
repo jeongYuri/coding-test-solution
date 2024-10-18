52e3cc6c68708567fc3eed0c50e6d8fafd41eaf3
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-10-18 13:44:47 +0900

    [level 2] Title: 시소 짝꿍, Time: 106.75 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,18 +1,14 @@
-from collections import defaultdict
-
+from collections import Counter
 def solution(weights):
     answer = 0
-    weights.sort()
-    count_map = defaultdict(int)
-    
-    for weight in weights:
-
-        answer += count_map[weight]       
-        count_map[weight] += 1
-        if weight % 2 == 0:
-            count_map[(weight // 2) * 3] += 1
-        if weight % 3 == 0:
-            count_map[(weight // 3) * 4] += 1
-        count_map[weight * 2] += 1
-    
+    cnt =Counter(weights)
+    for k, v in cnt.items():
+        if v>1:
+            answer += v *(v-1)//2
+        
+    weights = list(set(weights))
+    for item in weights:
+        for check in (3/4, 2/3,2/4):
+            if item * check in weights:
+                answer += cnt[item] *cnt[item*check]
     return answer
