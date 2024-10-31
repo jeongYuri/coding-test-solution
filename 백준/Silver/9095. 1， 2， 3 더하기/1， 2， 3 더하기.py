420ae17be4429298a0263cc3cfc8db9309cf6379
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-10-31 18:00:06 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,14 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 t = int(input())
-def solv(n):
-    if n==1:
-        return 1
-    elif n==2:
-        return 2
-    elif n==3:
-        return 4
-    else:
-        return solv(n-1)+solv(n-2)+solv(n-3)
 for _ in range(t):
     n = int(input())
-    result = solv(n)
-    print(result)
+    dp = [0] *(n+1)
+    if n >= 1:
+        dp[1] = 1
+    if n >= 2:
+        dp[2] = 2
+    if n >= 3:
+        dp[3] = 4
+
+    for i in range(4,n+1):
+        dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+    print(dp[n])
