d0f5b0834086589345e4b30f2517d5c9912de023
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-10-31 14:21:23 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,5 +1,4 @@
 def solution(n, lost, reserve):
-    answer = 0
     set_lost = set(lost)-set(reserve)
     set_reserve = set(reserve) - set(lost)
     for i in set_reserve:
@@ -7,4 +6,5 @@ def solution(n, lost, reserve):
             set_lost.remove(i-1)
         elif i+1 in set_lost:
             set_lost.remove(i+1)
+            
     return n-len(set_lost)
