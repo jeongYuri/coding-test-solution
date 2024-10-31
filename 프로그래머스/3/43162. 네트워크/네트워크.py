88d1a61ab8f8dbd8473816de6a64ee60d2768941
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-10-31 15:42:14 +0900

    [level 3] Title: 네트워크, Time: 1.29 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -3,11 +3,11 @@ def solution(n, computers):
     visited = [0 for _ in range(len(computers))]
     def dfs(i):
         visited[i] = 1
-        for j in range(n):
+        for j in range(len(computers)):
             if computers[i][j] and not visited[j]:
                 dfs(j)
     for i in range(n):
         if not visited[i]:
             dfs(i)
-            answer+=1
+            answer +=1
     return answer
