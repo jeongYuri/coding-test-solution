1a7e3bf81a884bf813facdec0d479c2ba50b7afe
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-11-01 20:49:06 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,15 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-text = list(input().rstrip())
-dic =  {chr(i): -1 for i in range(ord('a'), ord('z')+1)}
-for i in range(len(text)):
-    if text[i] in dic:
-        if dic[text[i]]==-1:
-            dic[text[i]]=i
-        else:
-            continue
-
-
-for i in dic.values():
-    print(i,end=' ')
+s = list(input())
+c = 'abcdefghijklmnopqrstuvwxyz'
+for i in c:
+    if i in s:
+        print(s.index(i),end = " ")
+    else:
+        print(-1, end=" ")
