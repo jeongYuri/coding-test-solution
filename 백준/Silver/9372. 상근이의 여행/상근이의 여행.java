cb6adc9d0edb2b9ae30d5017ba3084f25aeee1ab
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-12-23 15:58:43 +0900

    [Silver IV] Title: 상근이의 여행, Time: 932 ms, Memory: 234672 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i= 0; i<t;i++){
+          int n = sc.nextInt();
+          int m = sc.nextInt();
+          for(int j=0;j<m;j++){
+               int a = sc.nextInt();
+               int b = sc.nextInt();
+          }
+          System.out.println(n-1);
+        }
+        sc.close();
+        
+    }
+}
