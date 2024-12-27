ec83fd2e06095a7afb8e933c0dc1d376027b8770
yuri <74125993+jeongYuri@users.noreply.github.com>
2024-12-27 21:36:45 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 972 ms, Memory: 142076 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -1,10 +1,8 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-numbers = []
-for _ in range(n):
-    numbers.append(int(input()))
-numbers = sorted(numbers, reverse = True)
-for i in numbers:
-    print(i)
+n = int(input().strip())  #
+numbers = [int(input().strip()) for _ in range(n)]
+numbers.sort(reverse=True)
+
+print("\n".join(map(str, numbers)))
