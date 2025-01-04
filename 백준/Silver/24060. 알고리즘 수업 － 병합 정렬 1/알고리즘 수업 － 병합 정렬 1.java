f37091a56fea322a4d6bf143cb3367949fbd4d41
yuri <74125993+jeongYuri@users.noreply.github.com>
2025-01-04 15:04:19 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 596 ms, Memory: 74820 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -1,63 +1,53 @@
 import java.io.*;
 import java.util.*;
-import java.util.StringTokenizer;
 
-public class Main{
-    int [] A; //입력받는 배열
-    static int[] tmp; //정렬 후 저장하는 배열
-    static int res = -1; //결과 저장 (실패 시 -1)
-    static int cnt = 0; //저장 횟수 누적 저장
-    static int k; //저장 횟수
+public class Main {
+    static int[] tmp;   // 임시 배열
+    static int res = -1; 
+    static int cnt = 0; 
+    static int k;       // 저장 횟수 기준
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int n = Integer.parseInt(st.nextToken()); // 배열의 크기
-        k = Integer.parseInt(st.nextToken()); //저장 횟수
-        int[] a = new int[n];
-        tmp = new int[n]; //오름차순 정렬해서 저장할 배열 초기화
+        int n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        tmp = new int[n];
+        
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++){
-            a[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        merge_sort(a, 0, n-1);
+        
+        mergeSort(arr, 0, n - 1);
         System.out.println(res);
     }
-    static void merge_sort(int a[], int p, int r){
-        if(cnt>k) return;
-        if(p<r){
-            int q = (p+r)/2; //중간으로 쪼개기~
-            merge_sort(a, p, q);
-            merge_sort(a, q+1, r);
-            merge(a, p,q, r);
+
+    static void mergeSort(int[] arr, int left, int right){
+        if(cnt >= k) return;      // k번 저장을 이미 넘어가면 종료
+        if(left < right){
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
         }
     }
-    static void merge(int array[], int p , int q, int r){
-        int i = p;
-        int j = q+1;
-        int t = 0;
-        while(i<=q && j<=r){
-            if (array[i]<array[j]){
-                tmp[t++] = array[i++];
-            }else{
-                tmp[t++]  = array[j++];
-            }
-        }
-        while(i<=q){
-            tmp[t++] = array[i++];
 
-        }while(j<=r){
-            tmp[t++] = array[j++];
+    static void merge(int[] arr, int left, int mid, int right){
+        int i = left, j = mid + 1, t = 0;
+        while(i <= mid && j <= right) {
+            tmp[t++] = (arr[i] < arr[j]) ? arr[i++] : arr[j++];
         }
-        i = p;
+        while(i <= mid)   tmp[t++] = arr[i++];
+        while(j <= right) tmp[t++] = arr[j++];
+        
         t = 0;
-        while(i<=r){
-            cnt ++;
-            if(cnt ==k){
-                res = tmp[t];
-                break;
-            }
-            array[i++] = tmp[t++];
+        for(i = left; i <= right; i++){
+            cnt++;
+            if(cnt == k) res = tmp[t];
+            arr[i] = tmp[t++];
         }
     }
 }
