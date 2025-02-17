86832b0bd2d590a92c548d13e525168fd2e1a7be
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-02-17 18:34:25 +0900

    [Gold IV] Title: 부분합, Time: 100 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,26 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-n,s = map(int,input().split())
-arr = [int(x) for x in sys.stdin.readline().rstrip().split()]
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+
 left = 0
 right = 0
-length = 100000
-arr_sum = arr[0]
+arr_sum = 0  # 초기 합을 0으로 
+length = n + 1  # 초기 길이를 n+1로 
 
-while left<=right:
-    if arr_sum>=s:
-        length = min(length,right-left+1)
-        arr_sum -=arr[left]
-        left+=1
-    else:
-        right +=1
-        if right<n:
-            arr_sum +=arr[right]
-        else:
-            break
+while right < n:
+    arr_sum += arr[right]  # 오른쪽 값 추가
 
-if length == 100000:
-    print(0)
-else:
-    print(length)
+    while arr_sum >= s:  # 조건을 만족하면 최소 길이 갱신
+        length = min(length, right - left + 1)
+        arr_sum -= arr[left]  
+        left += 1  
+
+    right += 1  
+
+# 길이가 갱신되지 않았다면 0
+print(length if length != n + 1 else 0)
