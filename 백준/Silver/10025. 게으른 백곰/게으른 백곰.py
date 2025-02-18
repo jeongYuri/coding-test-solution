771b9a211728f772c8084e976d5f26c1ed704162
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-02-18 09:58:01 +0900

    [Silver III] Title: 게으른 백곰, Time: 392 ms, Memory: 52996 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
@@ -4,22 +4,19 @@ input = sys.stdin.readline
 
 n,k = map(int,input().split())
 
-max_x = 1_000_000
-bucket = [0]*(max_x+1)
+bucket = [0]*1000001
+last_idx = 0
 
 for _ in range(n):
     g,x = map(int,input().split())
-    bucket[x]+=g #좌표 x 에 얼음 g를 더함
+    bucket[x]=g #좌표 x 에 얼음 g를 더함
+    last_idx = max(last_idx, x)
 
-
-window = 2*k +1 #커버할 수 있는 범위 선택
-current_sum = sum(bucket[:window])
+size = 2*k +1 #커버할 수 있는 범위 선택
+current_sum = sum(bucket[:size])
 max_ice = current_sum
 
-for i in range(1, max_x - 2 * k + 1):
-    current_sum -= bucket[i - 1]
-    if i + 2 * k <= max_x:
-        current_sum += bucket[i + 2 * k]  # 오른쪽 끝 값 추가
-
-    max_ice = max(max_ice, current_sum)  # 최댓값 갱신
+for end in range(size,last_idx+1):
+    current_sum += bucket[end]-bucket[end-size]
+    max_ice = max(max_ice,current_sum)
 print(max_ice)
