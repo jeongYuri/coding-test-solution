2ad650def057d5ab353497c553510134a7a13626
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-03-22 15:50:18 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 384 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    x,y = map(int,input().split())
+    d = y-x
+    n = 0
+    while True:
+        if d<= n*(n+1):
+            break
+        n+=1
+    if d<= n**2:
+        print(n*2-1)
+    else:
+        print(n*2)
+
+
