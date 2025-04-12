5d50e20aaf0494026a7daeb63bf787e8ef75a1df
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-04-12 20:56:56 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
@@ -6,13 +6,13 @@ for _ in range(tc):
     n = int(input())
     arr = {}
     for _ in range(n):
-        name,liter = map(str,input().split())
-        if name not in arr:
-            arr[name] = []
-        arr[name].append(int(liter))
-    winner = max(sum(liter) for liter in arr.values())
-    for name in arr:
-        if sum(arr[name])== winner:
+        name, liter = input().split()
+        liter = int(liter)  
+        arr.setdefault(name, []).append(liter)  
+
+    winner = max(map(lambda x: sum(x), arr.values()))
+    
+    for name, liters in arr.items():
+        if sum(liters) == winner:
             print(name)
             break
-
