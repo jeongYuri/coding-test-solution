859182ea5dbb8fec59fb47654d2259af400be366
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-05-18 23:08:38 +0900

    [Bronze I] Title: 행복한지 슬픈지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10769. 행복한지 슬픈지/행복한지 슬픈지.py
@@ -0,0 +1,12 @@
+s = input()
+happy = s.count(':-)')
+sad = s.count(':-(')
+
+if happy == 0 and sad == 0:
+    print('none')
+elif happy > sad:
+    print('happy')
+elif happy < sad:
+    print('sad')
+else:
+    print('unsure')
