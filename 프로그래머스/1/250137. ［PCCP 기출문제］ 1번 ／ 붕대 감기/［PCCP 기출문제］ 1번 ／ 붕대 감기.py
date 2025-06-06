1e4bf58b7bb082ab66bd9e7fef4720c9fdabd231
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-06-06 10:59:08 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.29 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,25 +1,25 @@
-def solution(bandage, health, attacks):
-    t, x, y = bandage  
+def solution(bandage, health, attacks): #bandage는 붕대감기, health는 최대 체력, attacks는 몬스터
+    t, h, p = bandage
     max_health = health
     current_health = max_health
-    continuity = 0 
-
-    attack_dict = {time: damage for time, damage in attacks}
-
-    for time in range(1, max(attack_dict.keys()) + 1):
+    conti = 0
+    
+    attack_dict = {time:damage for time, damage in attacks}
+    
+    for time in range(1, max(attack_dict.keys())+1):
         if time in attack_dict:
             current_health -= attack_dict[time]
-            continuity = 0  
-            if current_health <= 0:
-                return -1  
+            conti = 0
+            if current_health<=0:
+                return -1
         else:
-            current_health += x
-            current_health = min(current_health, max_health)  # Cap at max_health
-            continuity += 1
-
-            if continuity == t:
-                current_health += y
-                current_health = min(current_health, max_health)
-                continuity = 0
-
+            current_health += h   
+            current_health = min(current_health, max_health)
+            conti +=1
+            
+            if conti == t:
+                current_health += p
+                current_health  = min(current_health, max_health)
+                conti = 0
     return current_health
+    
