f9e9d7b4186f7626cca473489c0944de1db7f30a
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-06-26 19:01:14 +0900

    [Bronze II] Title: 거스름돈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+change = 1000-n
+coins = [500,100,50,10,5,1]
+cnt = 0
+
+for coin in coins:
+    cnt += change//coin
+    change%=coin
+print(cnt)
