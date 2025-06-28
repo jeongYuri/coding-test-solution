22993fdd9b1b8b86e32ff6e9196f4fc631107ce4
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-06-28 21:41:28 +0900

    [Silver I] Title: 1로 만들기 2, Time: 620 ms, Memory: 79784 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * (n + 1)
+prev = [0] * (n + 1)
+
+for i in range(2, n + 1):
+    dp[i] = dp[i - 1] + 1
+    prev[i] = i - 1
+
+    if i % 2 == 0 and dp[i // 2] + 1 < dp[i]:
+        dp[i] = dp[i // 2] + 1
+        prev[i] = i // 2
+
+    if i % 3 == 0 and dp[i // 3] + 1 < dp[i]:
+        dp[i] = dp[i // 3] + 1
+        prev[i] = i // 3
+
+path = []
+current = n
+while current != 0:
+    path.append(current)
+    current = prev[current]
+
+print(dp[n])
+print(*path)
