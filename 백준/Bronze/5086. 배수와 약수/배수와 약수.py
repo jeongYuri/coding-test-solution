63573842504cf9b85e35b95ac80b906ed456fec9
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-08-13 18:15:08 +0900

    [Bronze III] Title: 배수와 약수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    a,b = map(int,input().split())
+    if a==0 and b==0:
+        break
+    if a%b==0:
+        print("multiple")
+    elif a%b!=0 and b%a==0:
+        print("factor")
+    else:
+        print("neither")
+
