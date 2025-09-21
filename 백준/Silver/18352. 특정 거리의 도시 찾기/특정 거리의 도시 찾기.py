93c0640a32ea074c85ea43f635c300b26bd256f2
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-09-21 15:15:40 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1556 ms, Memory: 99812 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,27 +1,31 @@
 import sys
 input = sys.stdin.readline
+n,m,k,x = map(int,input().split())
 from collections import deque
-n,m,k,x = map(int,input().split()) #도시의 개수, 도로의 개수, 거리 정보, 출발 도시의 번호
-graph = [[] for _ in range(n + 1)]
+dist = [[] for _ in range(n+1)]
+answer = []
+visited = [-1]*(n+1)
 
+def bfs(v):
+    q = deque()
+    q.append(v)
+    visited[v]+=1
+    while q:
+        now = q.popleft()
+        for i in dist[now]:
+            if visited[i]==-1:
+                visited[i] = visited[now]+1
+                q.append(i)
 for _ in range(m):
     a,b = map(int,input().split())
-    graph[a].append(b) #a번 도시에서 b번 도시로 이동하는 단방향..도로
+    dist[a].append(b)
 
-distance = [-1]*(n+1)
-distance[x] = 0 # 출발 도시의 거리는 0
-
-q = deque([x])
-
-while q:
-    now = q.popleft()
-    for next_city in graph[now]:
-        if distance[next_city]==-1:
-            distance[next_city] = distance[now]+1
-            q.append(next_city)
-res = [i for i in range(1,n+1) if distance[i]==k]
-if res:
-    for city in sorted(res):
-        print(city)
-else:
+bfs(x)
+for i in range(n+1):
+    if visited[i]==k:
+        answer.append(i)
+if not answer:
     print(-1)
+else:
+    for i in sorted(answer):
+        print(i)
