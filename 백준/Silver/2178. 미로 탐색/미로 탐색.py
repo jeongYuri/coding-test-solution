bbb4c93324e3c8ecc1bf9b4abe648da50c2ae86a
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-09-27 13:21:24 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,20 +1,28 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-def bfs(x, y):
-    q = deque()
-    q.append((x, y))
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1:
-                q.append((nx, ny))
-                board[nx][ny] = board[x][y] + 1
-    return board[n-1][m-1]
 n, m = map(int, input().split())
-board = [list(map(int, input().rstrip())) for _ in range(n)]
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-print(bfs(0,0))
+miro = [list(map(int, input().rstrip())) for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
+
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+def bfs(i,j):
+    q = deque()
+    q.append((i,j))
+    visited[i][j] = True   
+    while q:
+        x,y = q.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < n and 0 <= ny < m:
+                if miro[nx][ny] != 0 and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    miro[nx][ny] = miro[x][y] + 1
+                    q.append((nx,ny))
+
+bfs(0,0)
+print(miro[n-1][m-1])
