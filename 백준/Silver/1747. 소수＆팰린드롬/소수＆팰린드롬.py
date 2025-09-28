8ecd8a318f0dbd415bddbd34480aec99eb85b2a4
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-09-28 15:35:25 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 3528 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -1,32 +1,22 @@
 import sys
 import math
 input = sys.stdin.readline
-n = int(input())
-a =[0]*(10000001)
-for i in range(2,len(a)):
-    a[i] = i
 
-for i in range(2,int(math.sqrt(len(a)+1))):
-    if a[i]==0:
-        continue
-    for j in range(i+i,len(a),i):
-        a[j] = 0
-
-def ispalindrome(target):
-    temp = list(str(target))
-    s = 0
-    e = len(temp)-1
-    while(s<e):
-        if temp[s]!= temp[e]:
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(math.isqrt(x)) + 1):
+        if x % i == 0:
             return False
-        s+=1
-        e-=1
     return True
-i = n
+
+def is_palindrome(x):
+    s = str(x)
+    return s == s[::-1]
+
+n = int(input())
 while True:
-    if a[i]!=0:
-        res = a[i]
-        if(ispalindrome(res)):
-            print(res)
-            break
-    i+=1
+    if is_prime(n) and is_palindrome(n):
+        print(n)
+        break
+    n += 1
