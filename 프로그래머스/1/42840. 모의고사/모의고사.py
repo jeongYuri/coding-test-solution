4e77167848bdfedbcf9f8d00cd9beff0b39f5264
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-10-31 15:19:25 +0900

    [level 1] Title: 모의고사, Time: 5.55 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,18 +1,19 @@
 def solution(answers):
-    win = []
-    patterns = [
-        [1, 2, 3, 4, 5],          
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] 
-    ]
-    scores = [0]*len(patterns)
-    for i, answer in enumerate(answers):
-        for j, pattern in enumerate(patterns):
-            if answer == pattern[i%len(pattern)]:
-                scores[j]+=1
-    max_score = max(scores)
-    result = [] 
-    for i, score in enumerate(scores): 
-        if score == max_score: 
-            result.append(i + 1)    
-    return result
+    st1=  [1,2,3,4,5]
+    st2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    st3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ans = [0, 0, 0]
+    for i in range(len(answers)):
+        if st1[i%len(st1)]==answers[i]:
+            ans[0]+=1
+        if st2[i%len(st2)]==answers[i]:
+            ans[1]+=1
+        if st3[i%len(st3)]==answers[i]:
+            ans[2]+=1
+    max_score = max(ans)
+    res = []
+    for i in range(3):
+        if ans[i]==max_score:
+            res.append(i+1)
+    return res
+            
