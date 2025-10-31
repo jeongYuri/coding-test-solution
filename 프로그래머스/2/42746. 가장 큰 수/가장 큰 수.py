dc480a7fd0ee7db3a2ff96f282cfe28ce35e16de
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-10-31 15:29:36 +0900

    [level 2] Title: 가장 큰 수, Time: 1311.29 ms, Memory: 26 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,5 @@
 def solution(numbers):
-    answer = ''
-    big_num = [str(n) for n in numbers]
-    big_num.sort(key = lambda n:n*3,reverse = True)
-    return str(int(''.join(big_num)))
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    answer = ''.join(numbers)
+    return str(int(answer))
