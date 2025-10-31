af7285fe8d6b61530cabc0ab931e8acf121c8cfa
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-10-31 16:55:12 +0900

    [level 2] Title: 큰 수 만들기, Time: 117.79 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,9 +1,12 @@
 def solution(number, k):
     st = []
     for num in number:
-        while k and st and st[-1]<num:
+        while st and k>0 and st[-1]<num:
             st.pop()
             k-=1
         st.append(num)
-    st = st[:-k] if k else st
-    return ''.join(st)
+    if k>0:
+        return ''.join(st[:-k])
+    else:
+        return ''.join(st)
+    
