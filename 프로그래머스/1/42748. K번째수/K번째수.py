9d0c0ab3ed6a2b8121583b178c05cb4bd19c4794
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-10-31 14:11:29 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,6 @@
 def solution(array, commands):
-    answer = []
-    for com in commands:    
-        arra = array[com[0]-1:com[1]]
-        arr1  = sorted(arra)
-        arr2 = arr1[com[2]-1]
-        answer.append(arr2)
-    return answer
-   
+    ans = []
+    for x,y,z in commands:    
+        tmp = sorted(array[x-1:y])
+        ans.append(tmp[z-1])
+    return ans
