e2f84b796c3b78b8e407e006a6973a27e491c1f1
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-10-31 13:51:41 +0900

    [level 1] Title: 폰켓몬, Time: 0.76 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,5 +1,4 @@
 def solution(nums):
-    answer = 0
-    poketmon = set(nums) #중복 제거해서 간략하게 하기 위해
-    answer = min(len(poketmon),len(nums)//2)
-    return answer
+    unique = len(set(nums))
+    select = len(nums)//2
+    return min(unique, select)
