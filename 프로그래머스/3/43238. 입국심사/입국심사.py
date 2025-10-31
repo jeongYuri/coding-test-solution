6800f851eb89a68019f596d4194d7126fa304b4a
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-10-31 19:44:49 +0900

    [level 3] Title: 입국심사, Time: 564.98 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,14 +1,15 @@
 def solution(n, times):
-    answer = 0
-    start, end = 0, max(times)*n #가장 느린 심사관이 모든 사람 처리하는 경우 생각
-    while start<=end:
-        cnt = 0
-        mid = (start+end)//2
-        for time in times:
-            cnt += mid//time
-        if cnt>=n:
-            answer = mid
-            end = mid-1
-        else:
-            start = mid+1
+    left, right = 1, max(times) * n
+    answer = right
+
+    while left <= right:
+        mid = (left + right) // 2  
+        people = sum(mid // t for t in times)  
+
+        if people >= n: 
+            answer = mid  
+            right = mid - 1  
+        else: 
+            left = mid + 1
+
     return answer
