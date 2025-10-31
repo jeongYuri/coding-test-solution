14a4896fb2e961f3eea451abfba5747f1bc4723d
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-10-31 16:40:58 +0900

    [level 2] Title: 소수 찾기, Time: 5.50 ms, Memory: 9.57 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,23 +1,22 @@
-from itertools import permutations,combinations
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+from itertools import permutations
 
 def solution(numbers):
-    answer = 0
-    numbers = ''.join(map(str, numbers))
-    sosu = set()
+    nums = set()
     for i in range(1, len(numbers) + 1):
-        combi = permutations(numbers, i)
-        for p in combi:
-            num = int(''.join(p))
-            if is_prime(num):
-                sosu.add(num)
+        for p in permutations(numbers, i):
+            num = int(''.join(p))  
+            nums.add(num)
 
-    answer = len(sosu)
-    return answer
-    
+    def is_prime(n):
+        if n < 2:
+            return False
+        for i in range(2, int(n**0.5) + 1): 
+            if n % i == 0:
+                return False
+        return True
+
+    count = 0
+    for n in nums:
+        if is_prime(n):
+            count += 1
+    return count
