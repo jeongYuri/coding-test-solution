2431c2978da406a71baffbee21650732c32b7df6
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-08 15:56:02 +0900

    [level 1] Title: 햄버거 만들기, Time: 184.01 ms, Memory: 25.5 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,10 +1,9 @@
 def solution(ingredient):
-    count = 0
-    hab = []
-    for i in ingredient:
-        hab.append(i)
-        if hab[-4:]==[1,2,3,1]:
-            count +=1
-            for _ in range(4):
-                hab.pop()
-    return count
+    answer = 0
+    st = []
+    for ham in ingredient:
+        st.append(ham)
+        if st[-4:]==[1,2,3,1]:
+            answer+=1
+            del st[-4:]
+    return answer
