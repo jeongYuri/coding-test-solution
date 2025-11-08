e8ba59387bc23770ec20d318fe60210525d35c1d
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-08 18:41:47 +0900

    [level 1] Title: 바탕화면 정리, Time: 1.01 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,9 +1,13 @@
 def solution(wallpaper):
-    a,b = [],[]
-    for i in range(len(wallpaper)):
-        for j in range(len(wallpaper[i])):
-            if wallpaper[i][j]=="#":
-                a.append(i)
-                b.append(j)
-                
-    return [min(a),min(b),max(a)+1,max(b)+1]
+    min_row, min_col = float('inf'), float('inf')
+    max_row, max_col = 0, 0
+
+    for i in range(len(wallpaper)):       
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j] == '#':
+                min_row = min(min_row, i)
+                min_col = min(min_col, j)
+                max_row = max(max_row, i)
+                max_col = max(max_col, j)
+
+    return [min_row, min_col, max_row + 1, max_col + 1]
