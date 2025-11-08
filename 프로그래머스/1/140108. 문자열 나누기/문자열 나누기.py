14e6d6da5bb9553adde05ffe85a202e9fe87d2a5
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-08 12:04:07 +0900

    [level 1] Title: 문자열 나누기, Time: 0.99 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,18 +1,24 @@
 def solution(s):
-    answer = 1
-    start = s[0]
-    same = 0
-    differ = 0
-    
-    for i in s:
-        if differ != 0 and same ==differ:
-            answer +=1
-            same, differ = 0,0
-            start = i
-        if start==i:
-            same +=1
+    answer = 0
+    same, diff = 0, 0    
+    x = ''               
+
+    for ch in s:
+        if same == 0:   
+            x = ch
+            same = 1
+            diff = 0
         else:
-            differ +=1
-    
+            if ch == x:
+                same += 1
+            else:
+                diff += 1
         
+        if same == diff:  
+            answer += 1
+            same, diff = 0, 0 
+
+    if same != 0:
+        answer += 1
+
     return answer
