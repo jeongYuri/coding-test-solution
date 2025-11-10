a425cfb7345808f4ea4a146642d0c577c035b098
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-10 14:39:10 +0900

    [level 2] Title: 행렬의 곱셈, Time: 118.63 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,10 +1,8 @@
 def solution(arr1, arr2):
-    row = len(arr1)
-    col1 = len(arr1[0])
-    col2 = len(arr2[0])
-    answer = [[0]*col2 for _ in range(row)]
-    for i in range(row):
-        for j in range(col2):
-            for k in range(col1):
+    n,m,p = len(arr1),len(arr1[0]),len(arr2[0])
+    answer = [[0]*p for _ in range(n)]
+    for i in range(n):
+        for j in range(p):
+            for k in range(m):
                 answer[i][j] += arr1[i][k]*arr2[k][j]
     return answer
