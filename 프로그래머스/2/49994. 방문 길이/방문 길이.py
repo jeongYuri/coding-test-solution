a1e9e1f38522a29abfb4671f7f4f21711e480c19
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-10 19:00:36 +0900

    [level 2] Title: 방문 길이, Time: 0.40 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,16 +1,13 @@
 def solution(dirs):
-    answer = 0
-    Map = []
-    x,y = 5,5
-    move = {'U':(0,-1),'D':(0,1),'R':(1,0),'L':(-1,0)}
-    for i in range(len(dirs)):
-        (dx,dy) = move[dirs[i]]
-        if not(0<=x+dx and x+dx<=10 and 0<=y+dy and y+dy<=10):
-            continue
-        Map.append((x,y,x+dx,y+dy))
-        Map.append((x+dx,y+dy,x,y))
-        x = x + dx
-        y = y + dy
-        Mapset = set(Map)
-        answer = len(Mapset)//2
-    return answer
+    udrl = {'U':(0,1),'D':(0,-1),'R':(1,0),'L':(-1,0)}
+    x,y = 0,0
+    ans = set()
+    for d in dirs:
+        dx,dy = udrl[d]
+        nx, ny = x+dx, y+dy
+        if abs(nx)<=5 and abs(ny)<=5:
+            ans.add((x,y,nx,ny))
+            ans.add((nx,ny,x,y))
+            x = nx
+            y = ny
+    return len(ans)//2
