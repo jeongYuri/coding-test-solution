ba5367d552a1164974810e8938cfe586a9aecaf3
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-11 14:47:22 +0900

    [level 2] Title: 땅따먹기, Time: 410.68 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,6 +1,7 @@
 def solution(land):
-    answer = 0
-    for i in range(1,len(land)):
+    n = len(land)
+    for i in range(1,n):
         for j in range(4):
-            land[i][j] += max(land[i - 1][:j] + land[i - 1][j+1:])
+            land[i][j] += max(land[i-1][k] for k in range(4) if k!=j)
+
     return max(land[-1])
