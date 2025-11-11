18822c6ff2d0f5c97e8e4721bc6159951d2c6ab3
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-11 21:34:22 +0900

    [level 2] Title: 오픈채팅방, Time: 104.10 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,24 +1,21 @@
 def solution(record):
     answer = []
-    user_dict = {}
-    for r in record:
-        parts = r.split(" ")
-        if len(parts) < 2:
-            continue  
+    user = {}
+    for re in record:
+        parts = re.split()
         state = parts[0]
-        ids = parts[1]
-
-        if state in ['Enter', 'Change']:
-            name = parts[2]
-            user_dict[ids] = name
-    for r in record:
-        parts = r.split(" ")
+        uid= parts[1]
+        if state in ('Enter','Change'):
+            nickname = parts[2]
+            user[uid] = nickname
+    
+    for re in record:
+        parts = re.split()
         state = parts[0]
-        ids = parts[1]
-
+        uid = parts[1]
         if state == 'Enter':
-            answer.append(f"{user_dict[ids]}님이 들어왔습니다.")
+            answer.append(f"{user[uid]}님이 들어왔습니다.")
         elif state == 'Leave':
-            answer.append(f"{user_dict[ids]}님이 나갔습니다.")
+            answer.append(f"{user[uid]}님이 나갔습니다.")
 
     return answer
