6b094b06a784338c5d762f19c79ea376ff1a225b
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-11 23:32:58 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 286.85 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -1,17 +1,23 @@
 def solution(arr):
-    def dfs(x,y, size):
-        initial = arr[x][y]
-        for i in range(x,x+size):
-            for j in range(y,y+size):
-                if arr[i][j]!= initial:
-                    half = size//2 # 다른 값이 있으면 4개의 작은 영역으로 분할
-                    top_left = dfs(x,y,half)
-                    top_right = dfs(x,y+half, half)
-                    bottom_left = dfs(x+half, y, half)
-                    bottom_right = dfs(x+half, y+half, half)
-                    return [top_left[0] + top_right[0] + bottom_left[0] + bottom_right[0],
-                            top_left[1] + top_right[1] + bottom_left[1] + bottom_right[1]]
-        return [1,0] if initial ==0 else [0,1]
-    result = dfs(0, 0, len(arr))
-    
-    return result
+    answer = [0,0]
+    def compress(x,y,n):
+        first = arr[x][y]
+        same = True
+        for i in range(x,x+n):
+            for j in range(y,y+n):
+                if arr[i][j]!=first:
+                    same = False
+                    break
+            if not same:
+                break
+        if same:
+            answer[first]+=1
+            return
+        half = n//2
+        compress(x, y, half)
+        compress(x, y + half, half)
+        compress(x + half, y, half)
+        compress(x + half, y + half, half)
+            
+    compress(0, 0, len(arr))
+    return answer
