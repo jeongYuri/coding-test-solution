1b480cd5d2a6530b32142226e8baa9608ac722c5
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-11 22:13:11 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 185.60 ms, Memory: 22.9 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,10 +1,11 @@
 def solution(numbers):
     answer = []
-    for num in numbers:
-        bin_num = list('0'+ bin(num)[2:])
+    for number in numbers:
+        bin_num = list('0'+bin(number)[2:])
         idx = ''.join(bin_num).rfind('0')
         bin_num[idx] = '1'
-        if num %2==1:
+        if number%2==1:
             bin_num[idx+1] = '0'
-        answer.append(int(''.join(bin_num),2))
+        answer.append(int(''.join(bin_num), 2))
     return answer
+
