2a0d187d6b3df70a83692aa03fe7d79aaa4b5170
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-11 21:49:42 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 102.09 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,21 +1,41 @@
-def pop_num(b,m,n):
-    pop_set = set()
-    for i in range(1,n):
-        for j in range(1,m):
-            if b[i][j]==b[i-1][j-1]==b[i-1][j]==b[i][j-1]!='_':
-                pop_set |= set([(i, j), (i-1, j-1), (i-1, j), (i, j-1)])
-    for i,j in pop_set:
-        b[i][j] =0
-    for i, row in enumerate(b):
-        empty =['_']*row.count(0)
-        b[i] = empty + [block for block in row if block!=0]
-    return len(pop_set)
+def check(m, n, board):
+    filter = [[0] * n for _ in range(m)]
+    cnt = 0
+
+    for i in range(m - 1):
+        for j in range(n - 1):
+            a, b, c, d = board[i][j], board[i][j+1], board[i+1][j], board[i+1][j+1]
+            if a == b == c == d and a != '0':
+                filter[i][j] = filter[i][j+1] = filter[i+1][j] = filter[i+1][j+1] = 1
+
+    for i in range(m):
+        for j in range(n):
+            if filter[i][j] == 1:
+                board[i][j] = '0'
+                cnt += 1
+
+    if cnt == 0:
+        return 0  
+
+    for j in range(n):
+        stack = []
+        for i in range(m-1, -1, -1): 
+            if board[i][j] != '0':
+                stack.append(board[i][j])
+        for i in range(m-1, -1, -1):
+            board[i][j] = stack.pop(0) if stack else '0'
+
+    return cnt
+
 
 def solution(m, n, board):
-    cnt = 0
-    b = list(map(list,zip(*board)))
+    board = [list(row) for row in board]
+    answer = 0
+
     while True:
-        pop = pop_num(b,m,n)
-        if pop==0:
-            return cnt
-        cnt += pop
+        removed = check(m, n, board)
+        if removed == 0:
+            break
+        answer += removed
+
+    return answer
