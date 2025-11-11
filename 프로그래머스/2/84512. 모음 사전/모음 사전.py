20a748294bb2ec438f24def376527419ce1b35b5
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-11 13:00:43 +0900

    [level 2] Title: 모음 사전, Time: 1.95 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,6 +1,8 @@
+from itertools import product
 def solution(word):
-    answer = 0
-    for i, n in enumerate(word):
-        answer +=(5**(5-i)-1)/(5-1)*"AEIOU".index(n)+1
-    return answer
-    
+    words = []
+    for i in range(1,6):
+        for p in product("AEIOU",repeat = i):
+            words.append(''.join(p))
+    words.sort()
+    return words.index(word)+1
