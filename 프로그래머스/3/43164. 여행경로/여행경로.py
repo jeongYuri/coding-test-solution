c2066f8a78d40965590a0a42b8a6ada6886d63b7
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-12 16:28:01 +0900

    [level 3] Title: 여행경로, Time: 310.85 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,17 +1,16 @@
 def solution(tickets):
     tickets.sort()
-    visited =[False]*len(tickets)
+    visited = [False]*len(tickets)
     answer = []
     
     def dfs(current, path):
-        if len(path)==len(tickets)+1:
+        if len(path)== len(tickets)+1:
             answer.append(path)
             return
-        for i , (start, end) in enumerate(tickets):
+        for i, (start, end) in enumerate(tickets):
             if not visited[i] and start==current:
                 visited[i] = True
                 dfs(end, path+[end])
                 visited[i] = False
-    dfs("ICN", ["ICN"])
+    dfs('ICN',['ICN'])
     return answer[0]
- 
