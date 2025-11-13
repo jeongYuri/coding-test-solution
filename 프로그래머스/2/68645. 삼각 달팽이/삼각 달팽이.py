f627cad7e960fe06a7294f2ac98d3a054c65da80
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-13 12:57:12 +0900

    [level 2] Title: 삼각 달팽이, Time: 1800.60 ms, Memory: 44.5 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,19 +1,17 @@
 def solution(n):
-    triangle = [[0] * n for _ in range(n)]
-    directions = [(1, 0), (0, 1), (-1, -1)]  
+    arr = [[0]*(i+1) for i in range(n)]
+    x,y = -1,0
     num = 1
-    x, y = -1, 0
-    direction_idx = 0
-    for i in range(n):
-        for j in range(i, n):
-            dx, dy = directions[direction_idx]
-            x, y = x + dx, y + dy
-            triangle[x][y] = num
-            num += 1
-        direction_idx = (direction_idx + 1) % 3
-    answer = []
-    for row in triangle:
-        for num in row:
-            if num != 0:
-                answer.append(num)
-    return answer
+    for step in range(n):
+        for _ in range(step,n):
+            if step%3==0: #아래
+                x+=1
+            elif step%3==1: #오른쪽
+                y+=1
+            else: #왼쪽 위
+                x-=1
+                y-=1
+            arr[x][y] = num
+            num+=1
+        
+    return sum(arr,[])
