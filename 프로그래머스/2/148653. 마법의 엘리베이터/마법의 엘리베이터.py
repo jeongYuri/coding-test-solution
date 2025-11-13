948b5832926349a0315f09b2612eb41a46c82127
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-13 13:16:20 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,11 +1,21 @@
 def solution(storey):
-    cnt = 0
-    while storey>0:
-        digit = storey%10
-        if digit > 5 or(digit==5 and (storey//10)%10>4):
-            cnt += 10-digit
-            storey+=10
-        else:
-            cnt +=digit
-        storey//=10
-    return cnt
+    ans = 0
+    while storey > 0:
+        digit = storey % 10
+        next_digit = (storey // 10) % 10
+        
+        if digit > 5:
+            ans += 10 - digit
+            storey = storey // 10 + 1
+        elif digit < 5:
+            ans += digit
+            storey //= 10
+        else: 
+            if next_digit >= 5:
+                ans += 5
+                storey = storey // 10 + 1
+            else:
+                ans += 5
+                storey //= 10
+                
+    return ans
