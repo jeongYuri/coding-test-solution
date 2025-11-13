38bce4569d3cf59a3ec3bd06c76b450e7429410d
uri'ss <74125993+jeongYuri@users.noreply.github.com>
2025-11-13 14:01:36 +0900

    [level 2] Title: 호텔 대실, Time: 1.55 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,17 +1,16 @@
 import heapq
-def time_to(time_str:str)->str:
-    hh,mm = map(int,time_str.split(":"))
-    return hh*60+mm
-def solution(book_time:list)->int:
-    cnt = 0
+def to_min(time):
+    h, m = map(int, time.split(':'))
+    return h * 60 + m
+def solution(book_time):
+    answer = 0
     book_time.sort(key=lambda x:x[0])
-    heap=[]
+    rooms = []
     for start, end in book_time:
-        start_time = time_to(start)
-        end_time = time_to(end)+10 #청소 시간 10 분 추가
-        if heap and start_time>=heap[0]:
-            heapq.heappop(heap)
-        else:
-            cnt +=1
-        heapq.heappush(heap, end_time)
-    return cnt
+        s = to_min(start)
+        e = to_min(end) + 10 #청소 10분 추가
+        if rooms and rooms[0]<=s:
+            heapq.heappop(rooms)
+        heapq.heappush(rooms, e)
+            
+    return len(rooms)
